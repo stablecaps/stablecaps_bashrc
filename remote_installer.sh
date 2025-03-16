b594c6b450530b5fedb21e066b89f161bee518a6
Darkpandarts <14529342+darkpandarts@users.noreply.github.com>
2025-03-16 23:49:25 +0000

    correct remote installer

--- remote_installer.sh
+++ remote_installer.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-cd
+cd ${HOME}
 git clone https://github.com/stablecaps/stablecaps_bashrc.git ${HOME}/stablecaps_bashrc
 mv -f ${HOME}/.bashrc .your_old_bashrc
 ln -fs ${HOME}/stablecaps_bashrc/_bashrc ~/.bashrc
