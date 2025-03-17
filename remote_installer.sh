88a873d6443747c8bf81e27856ec088c0eaba7ab
Darkpandarts <14529342+darkpandarts@users.noreply.github.com>
2025-03-17 14:52:17 +0000

    correct remote installer 2

--- remote_installer.sh
+++ remote_installer.sh
@@ -2,7 +2,15 @@
 
 set -e 
 
-cd ${HOME}
+USERID=$(whoami)
+
+HOME="/home/${USERID}"
+if [ "$USERID" = "root" ]; then
+    HOME="/root"   
+fi
+
+
+cd $HOME
 git clone https://github.com/stablecaps/stablecaps_bashrc.git ${HOME}/stablecaps_bashrc
 mv -f ${HOME}/.bashrc .your_old_bashrc
 ln -fs ${HOME}/stablecaps_bashrc/_bashrc ~/.bashrc
